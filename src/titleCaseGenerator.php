fb458ee3013b0e82149845bb338afc0246fd53a6
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-02-12 20:20:32 -0800

    add exercise using specs test process.

---
+++ src/titleCaseGenerator.php
@@ -0,0 +1,21 @@
+<?php
+
+    class TitleCaseGenerator
+    {
+        // function makeTitleCase($input_title)
+        // {
+        //   return ucfirst($input_title);
+        //
+        // }
+        function makeTitleCase($input_title)
+        {
+            $input_array_of_words = explode(" ", $input_title);
+            $output_titlecased = array();
+            foreach ($input_array_of_words as $word) {
+                array_push($output_titlecased, ucfirst($word));
+            }
+            return implode(" ", $output_titlecased);
+        }
+    }
+
+?>
